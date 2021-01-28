a9467841e5519d742b521ec0f82ad6529e19c8ad
someone <someone@someplace.com>
2021-01-28 09:50:29 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+  </div>
 </template>
 
 <script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+
 export default {
   name: "App",
-  components: {},
+  components: { MainTabBar },
 };
 </script>
 
-<style></style>
+<style>
+@import "assets/css/base.css";
+</style>
---
+++ src/network/request.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+export function request(config) {
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: "http://123.207.32.32:8000",
+    timeout: 5000
+  });
+
+  // axios的拦截器
+  // 请求拦截作用
+  instance.interceptors.request.use(
+    config => {
+      return config;
+    },
+    err => {}
+  );
+  // 响应拦截
+  instance.interceptors.response.use(
+    res => {
+      return res.data;
+    },
+    err => {
+      console.log(err);
+    }
+  );
+  return instance(config);
+}
